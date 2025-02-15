0f380485cade3e5206400c793c067aca276e1dfe
kota0314 <kota0314yama@gmail.com>
2025-02-15 19:01:21 +0900

    add updata can data method

--- include/rabcl/interface/can.hpp
+++ include/rabcl/interface/can.hpp
@@ -12,6 +12,7 @@ class Can
 public:
     Can();
     ~Can();
+    static bool UpdateData(uint32_t idx, const uint8_t can_data[8], Info& data);
     static void Prepare2FloatData(float in_1, float in_2, uint8_t can_data[8]);
     static void Prepare1Float4IntData(float in_1, const uint8_t in_2[4], uint8_t can_data[8]);
 
--- src/interface/can.cpp
+++ src/interface/can.cpp
@@ -12,6 +12,67 @@ Can::~Can()
     // NOP
 }
 
+bool Can::UpdateData(uint32_t idx, const uint8_t can_data[8], Info& data)
+{
+    union {
+        float f;
+        int32_t ui;
+    } buf;
+    if (idx == (uint32_t)CAN_ID::CAN_CHASSIS_X_Y)
+    {
+        buf.ui = (int32_t) (
+            (((int32_t)can_data[0] << 24) & 0xFF000000)
+          | (((int32_t)can_data[1] << 16) & 0x00FF0000)
+          | (((int32_t)can_data[2] <<  8) & 0x0000FF00)
+          | (((int32_t)can_data[3] <<  0) & 0x000000FF)
+        );
+        data.chassis_vel_x_ = buf.f;
+        buf.ui = (int32_t) (
+              (((int32_t)can_data[4] << 24) & 0xFF000000)
+            | (((int32_t)can_data[5] << 16) & 0x00FF0000)
+            | (((int32_t)can_data[6] <<  8) & 0x0000FF00)
+            | (((int32_t)can_data[7] <<  0) & 0x000000FF)
+        );
+        data.chassis_vel_y_ = buf.f;
+    }
+    else if (idx == (uint32_t)CAN_ID::CAN_CHASSIS_Z_YAW)
+    {
+        buf.ui = (int32_t) (
+            (((int32_t)can_data[0] << 24) & 0xFF000000)
+          | (((int32_t)can_data[1] << 16) & 0x00FF0000)
+          | (((int32_t)can_data[2] <<  8) & 0x0000FF00)
+          | (((int32_t)can_data[3] <<  0) & 0x000000FF)
+        );
+        data.chassis_vel_z_ = buf.f;
+        buf.ui = (int32_t) (
+              (((int32_t)can_data[4] << 24) & 0xFF000000)
+            | (((int32_t)can_data[5] << 16) & 0x00FF0000)
+            | (((int32_t)can_data[6] <<  8) & 0x0000FF00)
+            | (((int32_t)can_data[7] <<  0) & 0x000000FF)
+        );
+        data.yaw_vel_ = buf.f;
+    }
+    else if (idx == (uint32_t)CAN_ID::CAN_PITCH_MODES)
+    {
+        buf.ui = (int32_t) (
+            (((int32_t)can_data[0] << 24) & 0xFF000000)
+          | (((int32_t)can_data[1] << 16) & 0x00FF0000)
+          | (((int32_t)can_data[2] <<  8) & 0x0000FF00)
+          | (((int32_t)can_data[3] <<  0) & 0x000000FF)
+        );
+        data.pitch_vel_ = buf.f;
+        data.load_mode_ = can_data[4 + (int)MODE_ID::LOAD];
+        data.fire_mode_ = can_data[4 + (int)MODE_ID::FIRE];
+        data.speed_mode_ = can_data[4 + (int)MODE_ID::SPEED];
+        data.chassis_mode_ = can_data[4 + (int)MODE_ID::CHASSIS];
+    }
+    else
+    {
+        return false;
+    }
+    return true;
+}
+
 void Can::Prepare2FloatData(float in_1, float in_2, uint8_t can_data[8])
 {
     union {
