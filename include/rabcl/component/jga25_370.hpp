7abd0d32c2ced378fbb14e09dc485ff69503398c
kota0314 <kota0314yama@gmail.com>
2025-11-02 21:29:12 +0900

    位置と速度制御をそれぞれ分けて書けるように変更

--- include/rabcl/component/jga25_370.hpp
+++ include/rabcl/component/jga25_370.hpp
@@ -12,33 +12,41 @@ private:
     static constexpr double ENCODER_SAMPLING_FREQUENCY = 0.2;
     static constexpr int NUM_ENCODER_DATA_BUF = 20; // motor:10ms, sampling_frequency:200ms
     static constexpr int NUM_ENCODER_RESOLUTION = 44;
-    static constexpr double MOTOR_CONTROL_P = 1.6;
-    static constexpr double MOTOR_CONTROL_D = 0.2;
+    static constexpr double MOTOR_CONTROL_POS_P = 0.1;
+    static constexpr double MOTOR_CONTROL_POS_D = 0.0;
+    static constexpr double MOTOR_CONTROL_VEL_P = 1.6;
+    static constexpr double MOTOR_CONTROL_VEL_D = 0.2;
 
 public:
     JGA25_370(
-        uint16_t motor_output_max, double reduction_ratio);
+        uint16_t motor_output_max, double reduction_ratio, uint8_t control_mode = 1);
     ~JGA25_370();
 
-    void SetCmdVel(double cmd_vel);
+    void SetCmdValue(double cmd_value);
     void SetEncoderCount(int16_t encoder_count);
     void UpdataEncoder();
     int16_t CalcMotorOutput();
 
-    double GetCmdVel();
+    double GetCmdValue();
     double GetActVel();
     double GetActPos();
+
+private:
+    int16_t CalcMotorPosOutput();
+    int16_t CalcMotorVelOutput();
 
 private:
     uint16_t motor_output_max_;
     double count_to_pos_factor_; // rad
     double count_to_vel_factor_; // rad/s
+    uint8_t control_mode_; // 0: pos, 1: vel
 
     std::vector<int16_t> encoder_count_buf_;
     int32_t encoder_count_sum_;
-    double pre_diff_; // rad
-    double cmd_vel_; // rad
-    double act_vel_; // rad
+    double pre_diff_; // rad or rad/s
+    double cmd_value_; // rad or rad/s
+    double act_pos_; // rad
+    double act_vel_; // rad/s
     double pre_output_;
 };
 } // namespace rabcl
--- src/component/jga25_370.cpp
+++ src/component/jga25_370.cpp
@@ -6,15 +6,14 @@
 namespace rabcl
 {
 JGA25_370::JGA25_370(
-    uint16_t motor_output_max, double reduction_ratio)
+    uint16_t motor_output_max, double reduction_ratio, uint8_t control_mode)
+:   motor_output_max_(motor_output_max), control_mode_(control_mode),
+    encoder_count_sum_(0), pre_diff_(0.0), cmd_value_(0.0), pre_output_(0.0)
 {
-    motor_output_max_ = motor_output_max;
-    count_to_pos_factor_ = 2.0 * M_PI / (NUM_ENCODER_RESOLUTION * reduction_ratio);
-    count_to_vel_factor_ = 2.0 * M_PI / (NUM_ENCODER_RESOLUTION * ENCODER_SAMPLING_FREQUENCY * reduction_ratio);
-    encoder_count_sum_ = 0;
-    pre_diff_ = 0.0;
-    cmd_vel_ = 0.0;
-    pre_output_ = 0.0;
+    count_to_pos_factor_ =
+        2.0 * M_PI / (NUM_ENCODER_RESOLUTION * reduction_ratio);
+    count_to_vel_factor_ =
+        2.0 * M_PI / (NUM_ENCODER_RESOLUTION * ENCODER_SAMPLING_FREQUENCY * reduction_ratio);
 }
 
 JGA25_370::~JGA25_370()
@@ -22,9 +21,9 @@ JGA25_370::~JGA25_370()
     // NOP
 }
 
-void JGA25_370::SetCmdVel(double cmd_vel)
+void JGA25_370::SetCmdValue(double cmd_val)
 {
-    cmd_vel_ = cmd_vel;
+    cmd_value_ = cmd_val;
 }
 
 void JGA25_370::SetEncoderCount(int16_t encoder_count)
@@ -40,16 +39,47 @@ void JGA25_370::UpdataEncoder()
         std::begin(encoder_count_buf_),
         std::end(encoder_count_buf_),
         std::remove_reference<decltype(*std::begin(encoder_count_buf_))>::type{});
+    act_pos_ = -1.0 * (double)encoder_count_sum_ * count_to_pos_factor_;
+    act_pos_ = std::fmod(act_pos_, 2.0 * M_PI);
+    if (act_pos_ < 2.0 * M_PI) {
+        act_pos_ += 2.0 * M_PI;
+    }
     act_vel_ = -1.0 * (double)encoder_count * count_to_vel_factor_;
 }
 
 int16_t JGA25_370::CalcMotorOutput()
 {
+    if (0 == control_mode_) {
+        return CalcMotorPosOutput();
+    } else if (1 == control_mode_) {
+        return CalcMotorVelOutput();
+    }
+    return 0;
+}
+
+double JGA25_370::GetCmdValue()
+{
+    return cmd_value_;
+}
+
+double JGA25_370::GetActVel()
+{
+    return act_vel_;
+}
+
+double JGA25_370::GetActPos()
+{
+    return act_pos_;
+}
+
+int16_t JGA25_370::CalcMotorPosOutput()
+{
     // TODO: 制御器入れる、とりあえず適当なPD制御入れとく
+    // TODO: -pi ~ piで値取らないとだめな気がする
     int16_t error;
-    double diff = cmd_vel_ - act_vel_;
+    double diff = cmd_value_ - act_pos_;
     error = (int16_t)(
-        MOTOR_CONTROL_P * diff + MOTOR_CONTROL_D * (diff - pre_diff_));
+        MOTOR_CONTROL_POS_P * diff + MOTOR_CONTROL_POS_D * (diff - pre_diff_));
     pre_diff_ = diff;
 
     int16_t output = pre_output_ + error;
@@ -70,19 +100,30 @@ int16_t JGA25_370::CalcMotorOutput()
     return output;
 }
 
-double JGA25_370::GetCmdVel()
+int16_t JGA25_370::CalcMotorVelOutput()
 {
-    return cmd_vel_;
-}
+    // TODO: 制御器入れる、とりあえず適当なPD制御入れとく
+    int16_t error;
+    double diff = cmd_value_ - act_vel_;
+    error = (int16_t)(
+        MOTOR_CONTROL_VEL_P * diff + MOTOR_CONTROL_VEL_D * (diff - pre_diff_));
+    pre_diff_ = diff;
 
-double JGA25_370::GetActVel()
-{
-    return act_vel_;
-}
+    int16_t output = pre_output_ + error;
 
-double JGA25_370::GetActPos()
-{
-    double pos = -1.0 * (double)encoder_count_sum_ * count_to_pos_factor_;
-    return std::fmod(pos, 2.0 * M_PI);
+    if (std::abs(output) > motor_output_max_)
+    {
+        if (output >= 0)
+        {
+            output = (int16_t)motor_output_max_;
+        }
+        else
+        {
+            output = -1.0 * (int16_t)motor_output_max_;
+        }
+    }
+    pre_output_ = output;
+
+    return output;
 }
 } // namespace rabcl
